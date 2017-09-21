bc3416732cb42b39ef8eebf79a95acdb5f374428
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-09-21 13:16:17 +0500

    готово

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,15 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Console;
 
 namespace Task10
 {
     class Program
     {
+        public static int Input(bool status) // ввод числа N
+        {
+            var number = 0; // переменная для числа
+            bool ok; // показатель корректности ввода
+            do
+            {
+                try
+                {
+                    number = Convert.ToInt32(ReadLine());
+                    if (status) // проверка значения количества элементов
+                    {
+                        if (number < 1)
+                        {
+                            WriteLine("Количество элементов в дереве должно быть больше 1, повторите ввод");
+                            ok = false;
+                        }
+                        else if (number > 100)
+                        {
+                            WriteLine("Введено слишком большое число, повторите ввод");
+                            ok = false;
+                        }
+                        else ok = true;
+                    }
+                    else ok = true;
+                }
+                catch (FormatException)
+                {
+                    WriteLine("Ошибка при вводе числа");
+                    ok = false;
+                }
+                catch (OverflowException)
+                {
+                    WriteLine("Ошибка при вводе числа");
+                    ok = false;
+                }
+            } while (!ok);
+            return number;
+        }
+
         static void Main(string[] args)
         {
+            WriteLine("Введите количество всех элементов в дереве: ");
+            int size = Input(true);
+            int[] arr = new int[size];
+            WriteLine("Выберите способ ввода элементов: \n1.Сгенерировать рандомно\n2.Ввести с клавиатуры");
+            var userAnswer = Input(false);
+            switch (userAnswer)
+            {
+                case 1:
+                    Random rnd = new Random();
+                    for (var i = 0; i < size; i++)
+                        arr[i] = rnd.Next(-100, 100);
+                    break;
+                case 2:
+                    for (var i = 0; i < size; i++)
+                    {
+                        WriteLine("Введите значение {0} элемента", i + 1);
+                        arr[i] = Input(false);
+                    }
+                    break;
+            }
+            
+            Tree t = new Tree();
+            Tree.Create(arr, t);
+            Tree.ShowTree(t.Root, size);
+            WriteLine("Количество ярусов в дереве: " + t.Length);
+            int[] k = t.NodesOnLevel();
+            for (var i = 0; i < t.Length; i++)
+                WriteLine("Количество вершин на {0} ярусе: {1}", i + 1, k[i]);
+            ReadKey(true);
         }
     }
 }
---
+++ Task10/Tree.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task10
+{
+    public class Tree //создание дерева
+    {
+        public class Node //узел дерева
+        {
+            public Node Left { get; set; } //указатели узла
+            public Node Right { get; set; }
+            public int value; //вставляемое значение
+
+            public Node(int val)
+            {
+                value = val; //конструктор заполняет узел значением
+                Left = null;
+                Right = null;
+            }
+
+            public int Length()
+            {
+                if (Left == null && Right == null)
+                    return 1;
+
+                if (Left == null)
+                    return Right.Length() + 1;
+                if (Right == null)
+                    return Left.Length() + 1;
+                return Math.Max(Right.Length(), Left.Length()) + 1;
+            }
+
+            public List<int> level(int i)
+            {
+                if(i == 0)
+                    return new List<int>{value};
+
+                var result = new List<int>(0);
+                if (Right != null)
+                    result.AddRange(Right.level(i - 1));
+                if (Left != null)
+                    result.AddRange(Left.level(i - 1));
+                return result;
+            }
+        }
+
+        public Node Root; //корень дерева
+        public int Length => Root.Length();
+
+        private int[] _nodesOnLevels;
+
+        public Tree() //конструктор (по умолчанию) создания дерева
+        {
+            Root = null; //при создании корень не определен
+        }
+        public Tree(int value)
+        {
+            Root = new Node(value); //если изначально задаём корневое значение
+        }
+
+        //нерекурсивное добавление
+        public void Add(int value) //узел и его значение
+        {
+            if (Root == null) //если элемента нет
+            {
+                Root = new Node(value); //добавляем 
+                return;
+            }
+
+            Node current = Root; //текущий равен корневому
+
+
+            bool added = false;
+            //обходим дерево
+            do
+            {
+                if (value >= current.value) //идём вправо
+                {
+                    if (current.Right == null)
+                    {
+                        current.Right = new Node(value);
+                        added = true;
+
+                    }
+                    else
+                    {
+                        current = current.Right;
+                    }
+
+                }
+                if (value < current.value) //идём влево
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = new Node(value);
+                        added = true;
+                    }
+                    else
+                    {
+                        current = current.Left;
+                    }
+                }
+            } while (!added);
+        }
+
+        public static void Create(int[] values, Tree t)
+        {
+            foreach (var t1 in values)
+            {
+                t.Add(t1);
+            }
+        }
+
+        public List<int> Level(int level)
+        {
+            return Root.level(level);
+        }
+        public int[] NodesOnLevel()
+        {
+            _nodesOnLevels = new int[Length];
+            var result = new List<int>(0);
+            for (int i = 0; i < Length; i++)
+            {
+                result.AddRange(Level(i));
+                _nodesOnLevels[i] = Root.level(i).Count;
+            }
+            return _nodesOnLevels;
+        }
+
+        public static void ShowTree(Node r, int size) // вывод дерева
+        {
+            if (r != null)
+            {
+                ShowTree(r.Left, size + 4);//переход к левому поддереву
+                for (int i = 0; i < size; i++) Console.Write(" ");
+                Console.WriteLine(r.value);
+                ShowTree(r.Right, size + 4);//переход к правому поддереву
+            }
+        }
+    }
+}
+
+
